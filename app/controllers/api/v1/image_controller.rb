af83318352349e7be3e9d59106d1bc7b26e14ff0
Chris Allbritton <cla.inbox@gmail.com>
2021-01-16 16:47:11 -0500

    Refactor image controller post-refactor to render JSON

--- app/controllers/api/v1/image_controller.rb
+++ app/controllers/api/v1/image_controller.rb
@@ -1,18 +1,7 @@
 class Api::V1::ImageController < ApplicationController
   def show
     location = params[:location]
-
-    conn = Faraday.new(url: "https://api.unsplash.com") do |f|
-      f.params['client_id'] = ENV["IMAGE_API_KEY"]
-      f.params['query'] = location
-      f.params['page'] = 1
-      f.params['per_page'] = 1
-    end
-
-    response = conn.get("/search/photos")
-
-    parsed = JSON.parse(response.body, symbolize_names: true)
-
-    require "pry"; binding.pry
+    image = ImageFacade.get_image(location)
+    render json: ImageSerializer.new(image)
   end
 end
