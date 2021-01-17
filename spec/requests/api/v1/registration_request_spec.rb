6230dce44d2cb1e57cd893d9c79ad8b58a110a15
Chris Allbritton <cla.inbox@gmail.com>
2021-01-17 08:22:53 -0500

    Add registration_request_spec for initial testing of registration endpoint

---
+++ spec/requests/api/v1/registration_request_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe "Registration" do
+  it "can register a new user" do
+    user_params = ({
+                    email: 'Chris@email.com',
+                    password: '1234',
+                    password_confirmation: '1234'
+                  })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/users", headers: headers, params: JSON.generate(user_params)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to be_a(Hash)
+    expect(parsed).to have_key(Data)
+    expect(parsed[:data]).to be_a(Hash)
+    expect(parsed[:data]).to have_key(Type)
+    expect(parsed[:data][:type]).to eq("users")
+    expect(parsed[:data]).to have_key(:id)
+    expect(parsed[:data][:id]).to be_a(Integer)
+    expect(parsed[:data]).to have_key(:attributes)
+    expect(parsed[:data][:attributes]).to have_key(:email)
+    expect(parsed[:data][:attributes][:email]).to be_a(String)
+    expect(parsed[:data][:attributes]).to have_key(:api_key)
+    expect(parsed[:data][:attributes][:api_key]).to be_a(String)
+
+    created_user = User.last
+    expect(created_user.email).to eq(user_params[:email])
+    expect(created_user.password).to eq(user_params[:password])
+    expect(created_user.password_confirmation).to eq(user_params[:password_confirmation])
+  end
+end
