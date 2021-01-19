8cb80bcb3a241f81f418c99266e71251b957f5cb
Chris Allbritton <cla.inbox@gmail.com>
2021-01-19 12:32:49 -0500

    Call munchie object via MunchiesFacade get_munchies method prior to serialization

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -4,16 +4,16 @@ class Api::V1::MunchiesController < ApplicationController
     destination = params[:end]
     food = params[:food]
 
-    route_data = MapService.get_route_data(origin, destination)
-    duration = RoadTripFacade.confirmed_destination(route_data)
-    time = Time.now.to_i + duration
-    travel_time = RoadTripFacade.confirmed_travel_time(route_data)
-    coords = MapService.get_location_coords(destination)
-    current_forecast = WeatherService.get_forecast_by_coords(coords)[:current]
+    # route_data = MapService.get_route_data(origin, destination)
+    # duration = RoadTripFacade.confirmed_destination(route_data)
+    # time = Time.now.to_i + duration
+    # travel_time = RoadTripFacade.confirmed_travel_time(route_data)
+    # coords = MapService.get_location_coords(destination)
+    # current_forecast = WeatherService.get_forecast_by_coords(coords)[:current]
+    #
+    # yelp_data = MunchiesService.get_munchies_data(destination, time, food)
 
-    yelp_data = MunchiesService.get_munchies_data(destination, time, food)
-
-    munchie = Munchie.new(yelp_data, travel_time, destination, current_forecast)
+    munchie = MunchiesFacade.get_munchies(origin, destination, food)
 
   end
 end
