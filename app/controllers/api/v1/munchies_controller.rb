fdbb4131acb69c2d973ea3295f0a1d0900918a9b
Chris Allbritton <cla.inbox@gmail.com>
2021-01-19 13:38:08 -0500

    Add more robust logic to test if any part of the uri is missing or nil

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -4,7 +4,15 @@ class Api::V1::MunchiesController < ApplicationController
     destination = params[:end_location]
     food = params[:food]
 
-    munchie = MunchiesFacade.get_munchies(origin, destination, food)
-    render json: MunchieSerializer.new(munchie)
+    if origin == "" || origin == nil
+      render json: { errors: "Missing start location" }, status: 400
+    elsif destination == "" || destination == nil
+      render json: { errors: "Missing end location" }, status: 400
+    elsif food == "" || food == nil
+      render json: { errors: "Missing cuisine type" }, status: 400
+    else
+      munchie = MunchiesFacade.get_munchies(origin, destination, food)
+      render json: MunchieSerializer.new(munchie)
+    end
   end
 end
