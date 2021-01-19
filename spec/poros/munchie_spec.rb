c160af371a4513ae609c93eb68b2a58b7ebc3b33
Chris Allbritton <cla.inbox@gmail.com>
2021-01-19 12:46:32 -0500

    Add test for address method in munchie spec

--- spec/poros/munchie_spec.rb
+++ spec/poros/munchie_spec.rb
@@ -28,4 +28,21 @@ RSpec.describe Munchie do
     expect(munchie.restaurant).to have_key(:address)
     expect(munchie.restaurant[:address]).to be_a(String)
   end
+
+  it ".address" do
+    origin = "Denver, CO"
+    destination = "Pueblo, CO"
+    food = "chinese"
+    route_data = MapService.get_route_data(origin, destination)
+    travel_time = RoadTripFacade.confirmed_travel_time(route_data)
+    coords = MapService.get_location_coords(destination)
+    current_forecast = WeatherService.get_forecast_by_coords(coords)[:current]
+    duration = RoadTripFacade.confirmed_destination(route_data)
+    time = Time.now.to_i + duration
+    yelp_data = MunchiesService.get_munchies_data(destination, time, food)
+    munchie = Munchie.new(yelp_data, travel_time, destination, current_forecast)
+
+    expect(munchie).to be_a(Munchie)
+    expect(munchie.address(yelp_data)).to be_a(String)
+  end
 end
