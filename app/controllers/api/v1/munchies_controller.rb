d010a3c1c95fc11b40cef85206bfe387d5da76f6
Chris Allbritton <cla.inbox@gmail.com>
2021-01-19 11:57:29 -0500

    Add travel time and munchie object build out

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -7,6 +7,9 @@ class Api::V1::MunchiesController < ApplicationController
     route_data = MapService.get_route_data(origin, destination)
     duration = RoadTripFacade.confirmed_destination(route_data)
     time = Time.now.to_i + duration
+    travel_time = RoadTripFacade.confirmed_travel_time(route_data)
+    coords = MapService.get_location_coords(destination)
+    current_forecast = WeatherService.get_forecast_by_coords(coords)[:current]
 
     conn = Faraday.new(url: 'https://api.yelp.com') do |f|
       f.headers['Authorization'] = ENV['YELP_API_KEY']
@@ -20,6 +23,8 @@ class Api::V1::MunchiesController < ApplicationController
     end
 
     yelp_data = JSON.parse(response.body, symbolize_names: true)
+
+    munchie = Munchie.new(yelp_data, travel_time, destination, current_forecast)
 
   end
 end
